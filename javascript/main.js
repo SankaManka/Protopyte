4ddaef509d5f585f05f9b5292275e49c1cdc43e7
SankaManka <margosem02@mail.ru>
2023-06-17 07:51:00 +0300

    asd

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,16 @@
-let mau = 0
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+console.log("sad")
+btn.onclick = function() {
+	sidebar.classList.toggle("active");
+}
+document.getElementById("#btn").click();
+document.getElementById("#btn").focus();
+
+function hideDiv() {
+	var div = document.getElementById("#block");
+	div.style.display = "none";}
+
+function hideDiv1() {
+var div = document.getElementById("#block1");
+div.style.display = "none";}
